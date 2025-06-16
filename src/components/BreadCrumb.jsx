eb8430a13621da2e45d4bee174ebfbfe3dd52cf2
Ayomide2604 <106402640+Ayomide2604@users.noreply.github.com>
2025-06-16 08:24:19 +0100

    added shop link to breadcrumb using current path to conditionally render it

--- src/components/BreadCrumb.jsx
+++ src/components/BreadCrumb.jsx
@@ -1,16 +1,26 @@
 import React from "react";
-import { FaAngleRight, FaHouse } from "react-icons/fa6";
+import { FaAngleRight, FaBagShopping, FaHouse } from "react-icons/fa6";
+import { Link, useLocation } from "react-router-dom";
 
 const BreadCrumb = ({ title }) => {
+	const currentPath = useLocation().pathname;
 	return (
 		<div className="breadcrumb-option">
 			<div className="container">
 				<div className="row">
 					<div className="col-lg-12">
-						<div className="breadcrumb__links">
-							<a href="/">
-								<FaHouse className="fa fa-home" /> Home
-							</a>
+						<div className="breadcrumb__links d-flex align-items-center">
+							<Link to="/">
+								<FaHouse className="ms-2" /> Home
+							</Link>
+							{currentPath !== "/shop" ? (
+								<>
+									<FaAngleRight />
+									<Link to="/shop" className="ms-2">
+										<FaBagShopping /> Shop
+									</Link>
+								</>
+							) : null}
 							<span>
 								<FaAngleRight /> {title}
 							</span>
