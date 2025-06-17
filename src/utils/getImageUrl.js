cd57ede0762284216462ef7f6e4d2f9dae85d6c8
Ayomide2604 <106402640+Ayomide2604@users.noreply.github.com>
2025-06-17 21:40:43 +0100

    updated get image function

--- src/utils/getImageUrl.js
+++ src/utils/getImageUrl.js
@@ -1,5 +1,5 @@
 const getImageUrl = (record, id, image) => {
-	const imageUrl = `http://127.0.0.1:8090/api/files/${record}/${id}/${image}`;
+	const imageUrl = `https://shoeporium-pocketbase.onrender.com/api/files/${record}/${id}/${image}`;
 	return imageUrl;
 };
 
